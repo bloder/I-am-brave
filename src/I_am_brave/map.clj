d21e86c41494f1ac3030bfc77a51ef6b7ae8b595
Daniel <wowmegadenis@hotmail.com>
2016-04-03 21:36:30 -0300

    Ok I'm noob

--- src/I_am_brave/map.clj
+++ src/I_am_brave/map.clj
@@ -2,7 +2,7 @@
 
 (defn map-phrase
   [initial-word]
-  (str initial-word + ", This is my name, idiot"))
+  (str initial-word ", This is my name, idiot"))
 
 (map map-phrase ["Daniel", "Bloder"])
 ;;=> "Daniel, this is my name, idiot" "Bloder, this is my name, idiot"
