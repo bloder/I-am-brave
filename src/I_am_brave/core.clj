b19d5a0dfd05f9ab10e26a376082038e3d9e9df5
Daniel <wowmegadenis@hotmail.com>
2016-04-09 12:59:37 -0300

    Add map example with programming to abstraction

--- src/I_am_brave/core.clj
+++ src/I_am_brave/core.clj
@@ -3,8 +3,9 @@
   (:use [I-am-brave.lambda :as lambda])
   (:use [I-am-brave.closure :as closure])
   (:use [I-am-brave.lexical :as lexical])
-  (:use [I-am-brave.loop :as loop-ns]))
+  (:use [I-am-brave.loop :as loop-ns])
+  (:use [I-am-brave.map :as map-ns]))
 
 (defn -main
   [& args]
-  ((loop-ns/other-boring-sum-function 2)))
+  (map map-ns/compare-ages map-ns/man-age map-ns/woman-age))
--- src/I_am_brave/map.clj
+++ src/I_am_brave/map.clj
@@ -6,3 +6,13 @@
 
 (map map-phrase ["Daniel", "Bloder"])
 ;;=> "Daniel, this is my name, idiot" "Bloder, this is my name, idiot"
+
+(def man-age [18 19 17 21])
+(def woman-age [16 15 21 25])
+(defn compare-ages
+  [man woman]
+  {(:man man
+   :woman woman)})
+
+(map map-ns/compare-ages map-ns/man-age map-ns/woman-age)
+;; ({:man 18, :woman 16} {:man 19, :woman 15} {:man 17, :woman 21} {:man 21, :woman 25})
