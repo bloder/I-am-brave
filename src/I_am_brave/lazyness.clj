180dab549fb08581f7d4ed3f9a28d887ef834f6b
Daniel <wowmegadenis@hotmail.com>
2016-04-16 17:21:09 -0300

    Add lazy seq example to lazyness ns

--- src/I_am_brave/lazyness.clj
+++ src/I_am_brave/lazyness.clj
@@ -4,3 +4,6 @@
 (let [foo (range 200)
       even (filter even? foo)
       bar (take 4 even)] bar)
+
+(concat (take 3 (repeat "ela kika")) ["na" "vara"])
+;; ("ela kika" "ela kika" "ela kika" "na" "vara")
