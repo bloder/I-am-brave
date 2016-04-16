d1f608e552884f1ac9e9798c91c8dbb3e89a27f6
Daniel <wowmegadenis@hotmail.com>
2016-04-16 17:22:03 -0300

    Add lazyness first func result in right place

--- src/I_am_brave/lazyness.clj
+++ src/I_am_brave/lazyness.clj
@@ -1,9 +1,9 @@
 (ns I-am-brave.lazyness)
 
-;; (0 2 4 6)
 (let [foo (range 200)
       even (filter even? foo)
       bar (take 4 even)] bar)
+;; (0 2 4 6)
 
 (concat (take 3 (repeat "ela kika")) ["na" "vara"])
 ;; ("ela kika" "ela kika" "ela kika" "na" "vara")
