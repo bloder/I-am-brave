3484db434ed29a7ed58e13bd0c3bfd591a4ea073
Daniel <wowmegadenis@hotmail.com>
2016-04-16 18:40:23 -0300

    Add apply example in apply ns

--- src/I_am_brave/apply_partial.clj
+++ src/I_am_brave/apply_partial.clj
@@ -2,3 +2,11 @@
 
 (apply max [1 2 3 4 7])
 ;; 7
+
+(defn lets-into
+  "Only a function that does the same thing that a into function"
+  [first second]
+  (apply conj first second))
+
+(lets-into [2 3] [4 5 6 7])
+;; [2 3 4 5 6 7]
