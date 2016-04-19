16b08063a6fbe9c24a512851444790ab2935deed
Daniel <wowmegadenis@hotmail.com>
2016-04-19 19:09:30 -0300

    Add recursive example in loop ns

--- src/I_am_brave/loop.clj
+++ src/I_am_brave/loop.clj
@@ -8,3 +8,14 @@
     (if(> count 3)
     (println "Ok i don't want to sum it anymore, idiot")
     (recur (inc count)))))
+
+(defn recur-this-shit
+  "Sum all values from a vec"
+   ([foo] (recur-this-shit foo 0))
+   ([foo bar]
+     (if (empty? foo)
+         bar
+     (recur-this-shit (rest foo) (+ (first foo) bar)))))
+
+(recur-this-shit [50 5 1])
+;; 56
