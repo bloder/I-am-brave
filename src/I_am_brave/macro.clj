b0105f66152de2b51782e99d4586d479cfd1eeb9
Daniel <wowmegadenis@hotmail.com>
2016-05-01 22:30:28 -0300

    Add macro example in macro ns
    Using quote

--- src/I_am_brave/macro.clj
+++ src/I_am_brave/macro.clj
@@ -28,4 +28,16 @@
   (inc (second numbers)))
 
 (crazy-sum-macro (+ 2 2))
-;; 3  
+;; 3
+
+(defmacro clojure-good-codes
+  [badcode goodcode]
+  (list 'do
+        (list 'println str "this is a bad code in clojure : "
+              (list 'quote badcode))
+        (list 'println str "this is a good code in clojure : "
+              (list 'quote goodcode))))
+
+(clojure-good-codes (1 + 1) (+ 2 2))
+;; this is a bad code in clojure :  (1 + 1)
+;; this is a good code in clojure :  (+ 2 2)
