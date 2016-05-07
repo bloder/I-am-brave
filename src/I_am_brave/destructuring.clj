86ba5f664db8a4b0310792c1dfb05102bc9fac91
Daniel <wowmegadenis@hotmail.com>
2016-05-07 15:58:50 -0300

    Add example to str-example in destructuring ns

--- src/I_am_brave/destructuring.clj
+++ src/I_am_brave/destructuring.clj
@@ -11,5 +11,8 @@
   [[First Second & Losers]]
   (str "Winner - " First " Second - " Second " Losers - " Losers))
 
+(str-example ["Daniel" "Bloder" "Troxa 1" "Troxa 2"])
+;; "Winner - Daniel Second - Bloder Losers - "Troxa 1 Troxa 2"
+
 (let [{:keys [bar]} {:foo 2 :bar 3}] bar)
 ;; 3
