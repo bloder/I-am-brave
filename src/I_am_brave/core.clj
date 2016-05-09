797157473a34fbd9ce0062b3aa492ef2b8fafb2f
Daniel <wowmegadenis@hotmail.com>
2016-05-09 19:12:45 -0300

    Organize core ns

--- src/I_am_brave/core.clj
+++ src/I_am_brave/core.clj
@@ -1,10 +1,3 @@
-(ns I-am-brave.core
-  (:use [I-am-brave.destructuring :as destructuring])
-  (:use [I-am-brave.lambda :as lambda])
-  (:use [I-am-brave.closure :as closure])
-  (:use [I-am-brave.lexical :as lexical])
-  (:use [I-am-brave.loop :as loop-ns])
-  (:use [I-am-brave.map :as map-ns])
-  (:use [I-am-brave.take-drop :as take-drop-ns]))
+(ns I-am-brave.core)
 
 (defn -main [& args] )
