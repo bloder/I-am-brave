a3b5731975d641ad74052182e4fad79780d23aeb
Daniel <wowmegadenis@hotmail.com>
2016-05-10 22:24:37 -0300

    Add example of anonymous func in lambda ns

--- src/I_am_brave/lambda.clj
+++ src/I_am_brave/lambda.clj
@@ -4,6 +4,9 @@
   "Anonymous Function example"
   (fn [x] (*(+ x x)(* 10 5))))
 
+(anonymous-function 2)
+;; 200
+
 (def other-anonymous-function
   "Other way to call anonymous function with params"
   (#(* % 2) 8))
